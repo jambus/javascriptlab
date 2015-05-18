688125a27788123288d139ab7140a6a1ef8cfd93
jambus <jambus2010@gmail.com>
2015-05-19 00:01:56 +0800

    1. Add angluarJS learning slide 3 II

---
+++ javascriptlab/public/angluarjs/js/controllers.js
@@ -0,0 +1,28 @@
+var slideControl = angular.module('slideControl', []);
+
+slideControl.controller('indexControl', ['$scope',
+    function($scope) {
+        $scope.greeting = {
+            text: 'Hello'
+        };
+        $scope.pageClass="hello";
+    }
+]);
+
+slideControl.controller('slideControl', ['$scope',
+    function($scope) {
+        $scope.pageClass="list";
+    }
+]);
+
+slideControl.controller('slideControl2', ['$scope',
+	 function($scope) {
+	     $scope.pageClass="list2";
+	 }
+ ]);
+
+slideControl.controller('slideControl3', ['$scope',
+ 	 function($scope) {
+ 	     $scope.pageClass="listmain";
+ 	 }
+]);
